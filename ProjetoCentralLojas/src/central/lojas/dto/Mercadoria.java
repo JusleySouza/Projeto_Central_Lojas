9d4182eae08dda9b6aeaf487025bd643a1cb6dd8
Jusley Souza <jusleysouza31@gmail.com>
2021-11-02 18:00:58 -0300

    Criação do objeto Mercadoria

---
+++ ProjetoCentralLojas/src/central/lojas/dto/Mercadoria.java
@@ -0,0 +1,50 @@
+package central.lojas.dto;
+
+public class Mercadoria {
+
+	private int id;
+	private String nome;
+	private String lote;
+	private int quantidade;
+	private double preco;
+	private String tamanho;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLote() {
+		return lote;
+	}
+	public void setLote(String lote) {
+		this.lote = lote;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getTamanho() {
+		return tamanho;
+	}
+	public void setTamanho(String tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	
+}
